7d583c5076830aeb0c9e38d32d7abadf8ba1e787
Cristian Atehortua <cristian25a32@gmail.com>
2026-02-02 14:29:20 -0500

    feat: add optional clear target directory feature

--- README.md
+++ README.md
@@ -1,19 +1,13 @@
 # github-action-push-to-another-repository
 
-> [!WARNING]
-> TL;DR: this repository is not maintained for any new development. I plan to
-> fix issues if they arise due to changes on GitHub, to minimise disruption of
-> existing usage.
->
-> My circumstances changed since I started the GitHub Action in 2020. I am not
-> able to add functionality to it or fix issues.
->
-> The action is 175 lines of shell script. Feel free to fork it and modify it
-> for your own use case.
->
-> If you create a fork that might replace this one, I will add a note in the
-> documentation and the README.md. Please, open an issue and I will do it.
+> The purpose of this fork is to make optional the deletion of the target-directory. 
 
 See the extensive documentation in https://cpina.github.io/push-to-another-repository-docs/ (includes examples, FAQ, troubleshooting, etc.).
 
 GitHub repository of the documentation: https://github.com/cpina/push-to-another-repository-docs
+
+## Additional options not listed in link above
+
+### `clear-target-directory` [optional]
+
+Boolean (`true`/`false`). Defaults to `false`. If it’s true the target directory will be cleared before copying the files.
--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,7 @@ TARGET_BRANCH="${9}"
 COMMIT_MESSAGE="${10}"
 TARGET_DIRECTORY="${11}"
 CREATE_TARGET_BRANCH_IF_NEEDED="${12}"
+CLEAR_TARGET_DIRECTORY="${13}"
 
 if [ -z "$DESTINATION_REPOSITORY_USERNAME" ]
 then
@@ -57,14 +58,7 @@ fi
 
 
 CLONE_DIR=$(mktemp -d)
-
-echo "[+] Git version"
-git --version
-
-echo "[+] Enable git lfs"
 git lfs install
-
-echo "[+] Cloning destination git repository $DESTINATION_REPOSITORY_NAME"
 
 # Setup git
 git config --global user.email "$USER_EMAIL"
@@ -73,6 +67,7 @@ git config --global user.name "$USER_NAME"
 # workaround for https://github.com/cpina/github-action-push-to-another-repository/issues/103
 git config --global http.version HTTP/1.1
 
+echo "[+] Cloning destination git repository $DESTINATION_REPOSITORY_NAME"
 {
 	git clone --single-branch --depth 1 --branch "$TARGET_BRANCH" "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
 } || {
@@ -90,9 +85,7 @@ git config --global http.version HTTP/1.1
 	echo "::error::(Note that if they exist USER_NAME and API_TOKEN is redacted by GitHub)"
 	echo "::error::Please verify that the target repository exist AND that it contains the destination branch name, and is accesible by the API_TOKEN_GITHUB OR SSH_DEPLOY_KEY"
 	exit 1
-
 }
-ls -la "$CLONE_DIR"
 
 TEMP_DIR=$(mktemp -d)
 # This mv has been the easier way to be able to remove files that were there
@@ -100,27 +93,18 @@ TEMP_DIR=$(mktemp -d)
 # including "." and with the exception of ".git/"
 mv "$CLONE_DIR/.git" "$TEMP_DIR/.git"
 
-# $TARGET_DIRECTORY is '' by default
-ABSOLUTE_TARGET_DIRECTORY="$CLONE_DIR/$TARGET_DIRECTORY/"
-
-echo "[+] Deleting $ABSOLUTE_TARGET_DIRECTORY"
-rm -rf "$ABSOLUTE_TARGET_DIRECTORY"
-
-echo "[+] Creating (now empty) $ABSOLUTE_TARGET_DIRECTORY"
-mkdir -p "$ABSOLUTE_TARGET_DIRECTORY"
-
-echo "[+] Listing Current Directory Location"
-ls -al
-
-echo "[+] Listing root Location"
-ls -al /
+if [ "$CLEAR_TARGET_DIRECTORY" = "true" ]
+then
+    echo "[+] Clearing target directory ($CLONE_DIR/$TARGET_DIRECTORY)"
+    rm -rf "$CLONE_DIR/$TARGET_DIRECTORY"
+    mkdir -p "$CLONE_DIR/$TARGET_DIRECTORY"
+fi
 
 mv "$TEMP_DIR/.git" "$CLONE_DIR/.git"
 
-echo "[+] List contents of $SOURCE_DIRECTORY"
+echo "[+] Content of $SOURCE_DIRECTORY"
 ls "$SOURCE_DIRECTORY"
 
-echo "[+] Checking if local $SOURCE_DIRECTORY exist"
 if [ ! -d "$SOURCE_DIRECTORY" ]
 then
 	echo "ERROR: $SOURCE_DIRECTORY does not exist"
@@ -139,14 +123,10 @@ echo "[+] Copying contents of source repository folder $SOURCE_DIRECTORY to fold
 cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/$TARGET_DIRECTORY"
 cd "$CLONE_DIR"
 
-echo "[+] Files that will be pushed"
-ls -la
-
 ORIGIN_COMMIT="https://$GITHUB_SERVER/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
 COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
 COMMIT_MESSAGE="${COMMIT_MESSAGE/\$GITHUB_REF/$GITHUB_REF}"
 
-echo "[+] Set directory is safe ($CLONE_DIR)"
 # Related to https://github.com/cpina/github-action-push-to-another-repository/issues/64
 git config --global --add safe.directory "$CLONE_DIR"
 
@@ -160,16 +140,14 @@ then
     git switch -c "$TARGET_BRANCH" || true
 fi
 
-echo "[+] Adding git commit"
 git add .
 
-echo "[+] git status:"
+echo "[+] Changes:"
 git status
 
-echo "[+] git diff-index:"
+echo "[+] commiting changes:"
 # git diff-index : to avoid doing the git commit failing if there are no changes to be commit
 git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
-echo "[+] Pushing git commit"
-# --set-upstream: sets de branch when pushing to a branch that does not exist
-git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
+echo "[+] Pushing changes"
+git push "$GIT_CMD_REPOSITORY" "$TARGET_BRANCH"
